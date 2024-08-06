82bf76cd463e3de00a4935bf7a62e6e9926fb215
Tony Ciccarone <tonyvciccarone@gmail.com>
2024-08-06 16:39:03 -0400

    Add approved chambers table migration

---
+++ database/migrations/2024_08_06_203733_create_approved_chambers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApprovedChambersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('approved_chambers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('random_code', 6);
+            $table->date('approved_date'); // Added approved_date column
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('approved_chambers');
+    }
+}
